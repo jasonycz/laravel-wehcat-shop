752591dbc713d78dd81ab06d971d394523aca5ed
yangchengzhi <jsonyang@163.com>
2018-09-24 07:13:38 +0000

    邮箱验证中间件

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,9 +1,9 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
-use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
@@ -15,7 +15,11 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'email_verified',
+    ];
+
+    protected $casts = [
+        'email_verified' => 'boolean',
     ];
 
     /**
@@ -26,4 +30,5 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
 }
