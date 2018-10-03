aaaa5472a6c1d6bac8252f910798840bf4f862a4
vagrant <vagrant@homestead>
2018-10-03 01:59:09 +0000

    用户界面 - 订单列表页

--- routes/web.php
+++ routes/web.php
@@ -47,6 +47,7 @@ Route::group(['middleware' => 'auth'], function () {
 
         // Order
         Route::post('orders', 'OrdersController@store')->name('orders.store');
+        Route::get('orders', 'OrdersController@index')->name('orders.index');
     });
     // 结束
 });
