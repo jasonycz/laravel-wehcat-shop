9f52ebcf37936d3e5d71bac889444fc3774aaa44
jasonyangchengzhi <jsonyang@d163.com>
2018-11-03 12:56:22 +0000

    完善支付后逻辑

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\OrderPaid;
 use App\Exceptions\InvalidRequestException;
 use App\Models\Order;
 use Carbon\Carbon;
@@ -62,6 +63,8 @@ class PaymentController extends Controller
             'payment_no' => $data->trade_no, // 支付宝订单号
         ]);
 
+        $this->afterPaid($order);
+
         return app('alipay')->success();
     }
 
@@ -111,6 +114,12 @@ class PaymentController extends Controller
             'payment_no' => $data->transaction_id,
         ]);
 
+        $this->afterPaid($order);
         return app('wechat_pay')->success();
+    }
+
+    protected function afterPaid(Order $order)
+    {
+        event(new OrderPaid($order));
     }
 }
