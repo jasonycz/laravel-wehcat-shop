b0a2f5d4843459ca49d16a1a312b956041b0ff32
jasonyangchengzhi <jsonyang@d163.com>
2019-01-06 03:41:50 +0000

    添加apidoc使用说明用例 ApiDocIllumiationController.php

--- app/Http/Controllers/ApiDocIlluminationController.php
+++ app/Http/Controllers/ApiDocIlluminationController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class TestController extends Controller
+class ApiDocIlluminationController extends Controller
 {
     /**
      * Display a listing of the resource.
